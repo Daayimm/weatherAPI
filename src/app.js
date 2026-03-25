a70dff877540ec6de70ca78b64e5aaa8b8408dc6
Daayim Mustapha <c24052620@cardiff.ac.uk>
2026-03-25 15:49:31 +0000

    add redis caching and rate limiting

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 
+
+
 const express = require('express')
 const app = express()
 app.use(express.json())
 
+const limiter = require('./middleware/rateLimiter')
+app.use(limiter)
+
 const weather = require('./routes/weather')
 app.use('/weather', weather)
+
+
 
 module.exports = app
 
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,18 +1,27 @@
 
 const axios = require('axios')
 
+const client = require('../cache/redis')
+
 async function getWeather(city){
+    const cached = await client.get(city)
+    if(cached){
+        return JSON.parse(cached)
+    }
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${process.env.WEATHER_API_KEY}&unitGroup=metric&include=current`
     let response = await axios.get(url)
     const d = response.data
-    return {
+    const data ={
         city: d.resolvedAddress,
         temprature : d.currentConditions.temp,
         rain : d.currentConditions.preciptype,
         minTemp : d.days[0].tempmin,
         maxTemp: d.days[0].tempmax
-
     }
+    await client.set(city,JSON.stringify(data),{EX: 43200})
+    return data
+            
+            
 }
 module.exports = { getWeather }
 
